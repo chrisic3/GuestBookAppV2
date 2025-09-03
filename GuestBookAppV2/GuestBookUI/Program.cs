4d3ada9fd53cf7afe5b0a537c6728e6b7fb22313
chrisic3 <chris_ic3@hotmail.com>
2025-09-03 16:49:23 +0000

    Update Program with Working Code

--- GuestBookAppV2/GuestBookUI/Program.cs
+++ GuestBookAppV2/GuestBookUI/Program.cs
@@ -1,9 +1,42 @@
 ﻿using GuestBookLibrary.Models;
 
-GuestModel guest = new GuestModel();
-guest.FirstName = "John";
-guest.LastName = "Tester";
-guest.MessageToHost = "Beautiful place.";
-guest.GuestCount = 4;
+bool moreGuests = false;
 
-Console.WriteLine(guest.GuestInfo);
+do
+{
+    GuestModel guest = new GuestModel();
+
+    Console.Write("Please enter your first name: ");
+    guest.FirstName = Console.ReadLine();
+
+    Console.Write("Please enter your last name: ");
+    guest.LastName = Console.ReadLine();
+
+    Console.Write("Please enter your message to the host: ");
+    guest.MessageToHost = Console.ReadLine();
+
+    bool isValidNumber = false;
+    do
+    {
+        Console.Write("Please enter the number of people in your group: ");
+        string groupCount = Console.ReadLine();
+        isValidNumber = int.TryParse(groupCount, out int number);
+
+        if (!isValidNumber)
+        {
+            Console.WriteLine("Invalid number.");
+        }
+    } while (!isValidNumber);
+
+    Console.Write("Are there more guests to add? (y/n): ");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "y")
+    {
+        moreGuests = true;
+    }
+    else
+    {
+        moreGuests = false;
+    }
+} while (moreGuests);
